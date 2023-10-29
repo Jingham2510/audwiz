08068fa25604f07c5106ad9562851de1246cbfe9
Jingham2510 <jsphingham@gmail.com>
2023-10-29 19:36:59 +0000

    cooley_tukey optimisation/fixing

--- src/dsp.c
+++ src/dsp.c
@@ -69,63 +69,33 @@ void draw_twave(Wave wave, Vector2 start_point, MainMenu *menu){
 //Sort of like a divide and conquer method
 float *cooley_tukey(float *x, int N ,int s){   
     
+      float *comb_dft = malloc(N * sizeof(float));
+
     //Trivial 1 frame DFT
     if(N==1){
         return x;
     }
-    else{      
+      
 
-        //Split the float array in half - recursive
-        float *a_dft = cooley_tukey(x, N/2, 2*s);
-        float *b_dft = cooley_tukey((x+s), N/2, 2*s);
+    //Split the float array in half - recursive
+    float *a_dft = cooley_tukey(x, N/2, 2*s);
+    float *b_dft = cooley_tukey((x+s), N/2, 2*s);
 
-        //Combine the DFTs into full DFT
-        for(int k = 0; k < N/2 - 1; k++){
-            //Access the float at space k in the "array"
-            float p = *(a_dft + k);
-            
-            //Exponential of (-2pi*(i) divided by N all multiplied by k) multiplied by the relevant dfted frame. 
-            float q = exp(((-2 * PI * I)/N) * k) * *(b_dft + k);
-            
-            *(a_dft + k) = p + q;
-            
-            *(b_dft + k) = p - q;           
-
-        }
-
-        //Combine the DFT arrays into one DFT
-        float *comb_dft = malloc(N * sizeof(float));
-
-        int a_cnt = 0;
-        int b_cnt = 0;
-
-        //Interlace the two arrays with one-another
-        for(int i = 0; i < N; i++){
-
-            if(i % 2 == 0){
-                *(comb_dft + i) = *(a_dft + a_cnt);
-                a_cnt++;
-            }
-            else{
-                *(comb_dft + i) = *(b_dft + b_cnt);
-                b_cnt++;
-            }
-        }
-
-        /*
-        //OLD VERSION OF COMBINATION - Doesnt put them in alternating order, just one after the other
-        memcpy(comb_dft, a_dft, N/2 * sizeof(float));
-        memcpy(comb_dft + N/2, b_dft, N/2 * sizeof(float));
-        */
+    //Combine the DFTs into full DFT
+    for(int k = 0; k < N/2 - 1; k++){
+        //Access the float at space k in the "array"
+        float p = *(a_dft + k);
         
+        //Exponential of (-2pi*(i) divided by N all multiplied by k) multiplied by the relevant dfted frame. 
+        float q = exp(((-2 * PI * I)/N) * k) * *(b_dft + k);
+        
+        comb_dft[k] = p + q;
+        
+        comb_dft[k + N/2] = p - q;
+    }          
 
-
-
-
-
-        return comb_dft;
-
-    }
+    
+    return comb_dft;   
 
 
 }
@@ -170,8 +140,10 @@ void draw_fwave(Wave wave, Vector2 start_point, MainMenu *menu){
             vec_array[i] = curr_vec;
         }
 
-        
-        log_points(vec_array, sizeof(vec_array));
+        free(fft_wave);
+
+
+        //log_points(vec_array, sizeof(vec_array));
 
         memcpy(menu->wave_points, vec_array, sizeof(vec_array));
         menu->wave_points_calced = 1;   
